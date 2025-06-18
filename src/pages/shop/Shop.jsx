68f45ae82db12bb1648b9266386413e1b025ffc9
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-06-18 16:14:53 -0400

    Added a fetch to the shop to get 15 products Also added error and loading contingencies

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,6 +1,28 @@
 import "./Shop.css"
+import { useState, useEffect } from "react"
 
 export default function Shop() {
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+    fetch("https://fakestoreapi.com/products?limit=15", { mode: "cors" })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((response) => console.log(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+
+  //ADD ERROR AND LOADING LOGIC
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>A network error was encountered</p>;
 
     return (
         <>
