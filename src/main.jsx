cd3ac03074ccebd93e4e29fab7c48983c98a08bd
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-06-18 10:24:09 -0400

    Creating pages and components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
+const router = createBrowserRouter([
+
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
