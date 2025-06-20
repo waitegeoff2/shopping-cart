ece5bb3dbba51faa677f1c9c9c37cdab6c704903
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-06-20 16:31:37 -0400

    Cleaning up files

--- src/pages/cart/ShoppingCart.jsx
+++ src/pages/cart/ShoppingCart.jsx
@@ -1,34 +1,8 @@
 import './ShoppingCart.css'
 import { useOutletContext } from 'react-router-dom';
 
-// Use DISPLAYCARDS code to display the content in the shopping cart array
-//ADD THIS AS A SIDEBAR?????
-// // src/App.js
-    // import React, { useState } from 'react';
-    // import Sidebar from './components/Sidebar';
-    // import './App.css'; // Add global styling if needed
+//REPLACE THIS WITH SIDEBAR CART
 
-    // function App() {
-    //   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-    //   const toggleSidebar = () => {
-    //     setIsSidebarOpen(!isSidebarOpen);
-    //   };
-
-    //   return (
-    //     <div className="App">
-    //       <button onClick={toggleSidebar}>Open Sidebar</button>
-    //       <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-    //       <div className="main-content">
-    //         {/* Your main application content */}
-    //         <h1>Welcome to my App!</h1>
-    //         <p>This is the main content area.</p>
-    //       </div>
-    //     </div>
-    //   );
-    // }
-
-    // export default App;
 
 export default function ShoppingCart() {
 
